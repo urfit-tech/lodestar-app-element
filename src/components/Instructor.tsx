602ad7a74d06985d81c38bd741a90ab89cc133e4
sunnylochun <lo@qraft.app>
2021-06-30 13:53:36 +0800

    [FEATURE] add data components

---
+++ src/components/Instructor.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import DefaultAvatar from '../images/icons/avatar.svg'
+import { ParagraphProps, TitleProps } from '../types/style'
+import { generateCustomParagraphStyle, generateCustomTitleStyle } from './common'
+
+const StyledInstructorBlock = styled.div`
+  padding: 1rem;
+`
+const StyledAvatar = styled.img<{ size?: number }>`
+  overflow: hidden;
+  background: #ccc;
+  display: block;
+  margin: 0 auto 0.75rem;
+  width: ${props => props.size || 56}px;
+  height: ${props => props.size || 56}px;
+  border-radius: 50%;
+  box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.1);
+  object-fit: cover;
+  object-position: center;
+`
+const StyledSubTitle = styled.h2<{ customStyle?: TitleProps }>`
+  color: var(--gray-darker);
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 1.3;
+  letter-spacing: 0.77;
+  && {
+    ${generateCustomTitleStyle}
+  }
+`
+const StyledDescription = styled.div<{ customStyle?: ParagraphProps }>`
+  line-height: 1;
+  letter-spacing: 0.2px;
+  max-width: 190px;
+  text-align: center;
+  margin: 0 auto;
+  max-height: 190px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 8;
+  && {
+    ${generateCustomParagraphStyle}
+  }
+`
+const StyledAbstract = styled(StyledDescription)`
+  color: #a9a9a9;
+  line-height: 1.5;
+`
+
+const Instructor: React.FC<{
+  id: string | null
+  name: string | null
+  abstract: string | null
+  description: string | null
+  avatarUrl: string | null
+  isShowDescription?: boolean
+  customStyle?: {
+    subTitle?: TitleProps
+    abstract?: ParagraphProps
+    description?: ParagraphProps
+  }
+}> = ({ id, name, abstract, description, avatarUrl, isShowDescription, customStyle }) => {
+  return (
+    <StyledInstructorBlock key={id}>
+      <Link to={`/creators/${id}`}>
+        <div className="mb-4">
+          <StyledAvatar
+            src={avatarUrl !== null ? avatarUrl : DefaultAvatar}
+            alt={name || ''}
+            className="mx-auto"
+            size={128}
+          />
+        </div>
+        <StyledSubTitle customStyle={customStyle?.subTitle}>{name}</StyledSubTitle>
+        <StyledAbstract customStyle={customStyle?.abstract}>{abstract}</StyledAbstract>
+        {isShowDescription && (
+          <StyledDescription className="mt-3" customStyle={customStyle?.description}>
+            {description}
+          </StyledDescription>
+        )}
+      </Link>
+    </StyledInstructorBlock>
+  )
+}
+
+export default Instructor
---
+++ src/components/cards/ProjectCard.tsx
@@ -0,0 +1,128 @@
+import moment from 'moment'
+import { Circle } from 'rc-progress'
+import React, { useContext } from 'react'
+import { useIntl } from 'react-intl'
+import { Link } from 'react-router-dom'
+import styled, { ThemeContext } from 'styled-components'
+import { projectMessages } from '../../helpers/translation'
+import EmptyCover from '../../images/empty-cover.png'
+import { ReactComponent as CalendarAltOIcon } from '../../images/icons/calendar-alt-o.svg'
+import { ReactComponent as UserOIcon } from '../../images/icons/user-o.svg'
+import { ProjectBasicProps } from '../../types/data'
+import Card from '../Card'
+import { CustomRatioImage } from '../Image'
+import PriceLabel from '../label/priceLabel'
+
+const StyledLabel = styled.div`
+  color: ${props => props.theme['@primary-color']};
+`
+const StyledCircleWrapper = styled.div`
+  position: relative;
+`
+const StyledPercent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  color: var(--gray-darker);
+`
+export type ProjectCardProps = {
+  project: ProjectBasicProps
+}
+
+const ProjectCard: React.VFC<ProjectCardProps> = ({ project }) => {
+  const { formatMessage } = useIntl()
+  const theme = useContext(ThemeContext)
+
+  return (
+    <Link to={`/projects/${project.id}`}>
+      <Card
+        customStyle={{
+          direction: 'column',
+          bordered: false,
+          shadow: true,
+          p: '0',
+        }}
+      >
+        <CustomRatioImage width="100%" ratio={9 / 16} src={project.previewUrl || project.coverUrl || EmptyCover} />
+        <Card.ContentBlock>
+          <Card.Title>{project.title}</Card.Title>
+          <Card.Description>{project.abstract}</Card.Description>
+          <Card.MetaBlock className="d-flex align-items-end justify-content-between">
+            <StyledCircleWrapper>
+              {project.type === 'funding' && (
+                <StyledPercent>
+                  {!project.targetAmount
+                    ? 0
+                    : Math.floor(
+                        ((project.targetUnit === 'participants' ? project.enrollmentCount : project.totalSales) * 100) /
+                          project.targetAmount,
+                      )}
+                  %
+                </StyledPercent>
+              )}
+              {project.type === 'funding' ? (
+                <Circle
+                  percent={
+                    !project.targetAmount
+                      ? 0
+                      : Math.floor(
+                          ((project.targetUnit === 'participants' ? project.enrollmentCount : project.totalSales) *
+                            100) /
+                            project.targetAmount,
+                        )
+                  }
+                  style={{ width: 50 }}
+                  trailWidth={12}
+                  trailColor="#ececec"
+                  strokeWidth={12}
+                  strokeColor={theme['@primary-color']}
+                />
+              ) : project.isParticipantsVisible ? (
+                <div className="d-flex align-items-center">
+                  <UserOIcon />
+                  {formatMessage(projectMessages.text.people, { count: project.enrollmentCount })}
+                </div>
+              ) : null}
+            </StyledCircleWrapper>
+
+            <div className="text-right">
+              {project.type === 'funding' && (
+                <StyledLabel>
+                  {project.targetUnit === 'participants' &&
+                    formatMessage(projectMessages.text.totalParticipants, { count: project.enrollmentCount })}
+                  {project.targetUnit === 'funds' && <PriceLabel listPrice={project.totalSales || 0} />}
+                </StyledLabel>
+              )}
+              {project.isCountdownTimerVisible && project.expiredAt && (
+                <>
+                  {moment().isAfter(project.expiredAt) ? (
+                    <div className="d-flex align-items-center justify-content-end">
+                      <CalendarAltOIcon className="mr-1" />
+                      {project.type === 'funding'
+                        ? formatMessage(projectMessages.label.isExpiredFunding)
+                        : formatMessage(projectMessages.label.isExpired)}
+                    </div>
+                  ) : (
+                    <StyledLabel className="d-flex align-items-center">
+                      <CalendarAltOIcon className="mr-1" />
+                      {project.type === 'funding'
+                        ? formatMessage(projectMessages.text.fundingCountDownDays, {
+                            days: moment(project.expiredAt).diff(new Date(), 'days'),
+                          })
+                        : formatMessage(projectMessages.text.preOrderCountDownDays, {
+                            days: moment(project.expiredAt).diff(new Date(), 'days'),
+                          })}
+                    </StyledLabel>
+                  )}
+                </>
+              )}
+            </div>
+          </Card.MetaBlock>
+        </Card.ContentBlock>
+      </Card>
+    </Link>
+  )
+}
+
+export default ProjectCard
