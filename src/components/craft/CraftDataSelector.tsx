5674b07157f493694092c7d77b37d15457fcae51
Farrant <pcsh810778@gmail.com>
2021-08-26 11:15:02 +0800

    [FEATURE] add ContentSelector

--- src/components/craft/CraftDataSelector.tsx
+++ src/components/craft/CraftDataSelector.tsx
@@ -1,20 +1,18 @@
 import { useNode, UserComponent } from '@craftjs/core'
-import { Button, Collapse, Select } from 'antd'
+import { Button, Collapse } from 'antd'
 import Form from 'antd/lib/form/'
 import { useForm } from 'antd/lib/form/Form'
-import React, { useState } from 'react'
+import React from 'react'
 import { useIntl } from 'react-intl'
 import { commonMessages, craftPageMessages } from '../../helpers/translation'
-import { AdminHeaderTitle, StyledCollapsePanel, StyledCraftSettingLabel, StyledSettingButtonWrapper } from '../common'
+import { AdminHeaderTitle, StyledCollapsePanel, StyledSettingButtonWrapper } from '../common'
+import ContentSelector, { ContentType } from '../ContentSelector'
 
-type FieldProps = {
-  data?: string
-}
-
-const CraftDataSelector: UserComponent<{ setActiveKey: React.Dispatch<React.SetStateAction<string>> }> = ({
-  children,
-  setActiveKey,
-}) => {
+const CraftDataSelector: UserComponent<{
+  contentType: ContentType
+  customContentIds?: string[]
+  setActiveKey: React.Dispatch<React.SetStateAction<string>>
+}> = ({ contentType, customContentIds, children, setActiveKey }) => {
   const {
     connectors: { connect },
   } = useNode()
@@ -25,28 +23,29 @@ const CraftDataSelector: UserComponent<{ setActiveKey: React.Dispatch<React.SetS
       style={{ padding: '20px', background: '#dfa', cursor: 'pointer' }}
       onClick={() => setActiveKey('settings')}
     >
+      {/* {contentType === 'program' ? <ProgramBlock customContentIds={customContentIds} /> : null} */}
       {children}
     </div>
   )
+}
+type FieldProps = {
+  contentIds?: string[]
 }
 
 const DataSelectorSettings: React.VFC = () => {
   const { formatMessage } = useIntl()
   const [form] = useForm<FieldProps>()
-  const [selectedValue, setSelectedValue] = useState('')
 
   const {
     actions: { setProp },
     props,
     selected,
   } = useNode(node => ({
-    props: node.data.props as FieldProps,
+    props: node.data.props,
     selected: node.events.selected,
   }))
 
-  const handleSubmit = (values: FieldProps) => {
-    setProp(props => (props.data = values.data))
-  }
+  const handleSubmit = (values: FieldProps) => setProp(props => (props.customContentIds = values.contentIds))
 
   return (
     <Form
@@ -54,9 +53,7 @@ const DataSelectorSettings: React.VFC = () => {
       layout="vertical"
       colon={false}
       requiredMark={false}
-      initialValues={{
-        data: undefined,
-      }}
+      initialValues={{ contentIds: props.customContentIds }}
       onFinish={handleSubmit}
     >
       <Collapse
@@ -70,43 +67,8 @@ const DataSelectorSettings: React.VFC = () => {
           key="imageSetting"
           header={<AdminHeaderTitle>{formatMessage(craftPageMessages.label.specifyDisplayItem)}</AdminHeaderTitle>}
         >
-          <Form.Item
-            name="data"
-            label={
-              <StyledCraftSettingLabel>{formatMessage(craftPageMessages.label.dataDisplay)}</StyledCraftSettingLabel>
-            }
-          >
-            <Select
-              showSearch
-              allowClear
-              placeholder={formatMessage(craftPageMessages.label.choiceData)}
-              value={selectedValue}
-              onChange={(v: string) => setSelectedValue(v)}
-              filterOption={(input, option) =>
-                option?.props?.children
-                  ? (option.props.children as string).toLowerCase().indexOf(input.toLowerCase()) >= 0
-                  : true
-              }
-            >
-              <Select.Option key="program" value="program">
-                {formatMessage(craftPageMessages.label.program)}
-              </Select.Option>
-              <Select.Option key="activity" value="activity">
-                {formatMessage(craftPageMessages.label.activity)}
-              </Select.Option>
-              <Select.Option key="podcast" value="podcast">
-                {formatMessage(craftPageMessages.label.podcast)}
-              </Select.Option>
-              <Select.Option key="lecturer" value="lecturer">
-                {formatMessage(craftPageMessages.label.lecturer)}
-              </Select.Option>
-              <Select.Option key="fundraising" value="fundraising">
-                {formatMessage(craftPageMessages.label.fundraising)}
-              </Select.Option>
-              <Select.Option key="preOrder" value="preOrder">
-                {formatMessage(craftPageMessages.label.preOrder)}
-              </Select.Option>
-            </Select>
+          <Form.Item name="contentIds">
+            <ContentSelector contentType={props.contentType} />
           </Form.Item>
         </StyledCollapsePanel>
       </Collapse>
