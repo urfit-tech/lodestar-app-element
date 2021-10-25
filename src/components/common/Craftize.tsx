a0c3ebee7f2a47093e2d9fa1577feb5b636bc5b2
徐愷 <kkshyu.tw@gmail.com>
2021-10-24 21:01:42 -0700

    [BUGFIX] deconstruct types

--- src/components/common/Craftize.tsx
+++ src/components/common/Craftize.tsx
@@ -1,5 +1,4 @@
 import { useEditor, useNode, UserComponent } from '@craftjs/core'
-import { PropsWithChildren } from 'react-router/node_modules/@types/react'
 import styled, { css, CSSObject } from 'styled-components'
 import { ElementBaseProps, ElementComponent } from '../../types/element'
 
@@ -28,25 +27,26 @@ export const CraftSelectedMixin = css`
 // FIXME: why cannot use <P extends {editing?:boolean}> directly?
 // FIXME: only accept for FC and CC, not VFC
 const Craftize = <P extends object>(WrappedComponent: ElementComponent<P>) => {
-  const Component: UserComponent<ElementBaseProps<P> & PropsWithChildren<{ customStyle?: CSSObject }>> = props => {
-    const { editing } = useEditor(state => ({
-      editing: state.options.enabled,
-    }))
-    const {
-      connectors: { connect },
-      selected,
-      hovered,
-    } = useNode(node => ({
-      selected: node.events.selected,
-      hovered: node.events.hovered,
-    }))
-    const StyledCraftElement = styled(WrappedComponent)({ ...props.customStyle }) as ElementComponent<P>
-    return (
-      <CraftRefBlock ref={ref => ref && connect(ref)} events={{ hovered, selected }} options={{ enabled: editing }}>
-        <StyledCraftElement {...props} editing={editing} />
-      </CraftRefBlock>
-    )
-  }
+  const Component: UserComponent<ElementBaseProps<P> & { customStyle?: CSSObject; children?: React.ReactNode }> =
+    props => {
+      const { editing } = useEditor(state => ({
+        editing: state.options.enabled,
+      }))
+      const {
+        connectors: { connect },
+        selected,
+        hovered,
+      } = useNode(node => ({
+        selected: node.events.selected,
+        hovered: node.events.hovered,
+      }))
+      const StyledCraftElement = styled(WrappedComponent)({ ...props.customStyle }) as ElementComponent<P>
+      return (
+        <CraftRefBlock ref={ref => ref && connect(ref)} events={{ hovered, selected }} options={{ enabled: editing }}>
+          <StyledCraftElement {...props} editing={editing} />
+        </CraftRefBlock>
+      )
+    }
   return Component
 }
 
