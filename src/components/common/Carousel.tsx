ecebf4489cdcd2db3519730d25ee5f12cafbb9e5
徐愷 <kkshyu.tw@gmail.com>
2021-10-25 11:34:25 -0700

    [BUGFIX] simplify carousel

--- src/components/common/Carousel.tsx
+++ src/components/common/Carousel.tsx
@@ -1,10 +1,8 @@
-import { useHistory } from 'react-router'
-import Slider from 'react-slick'
+import Slider, { Settings as SliderProps } from 'react-slick'
 import styled from 'styled-components'
 import { ElementComponent } from '../../types/element'
-import Slide from './Slide'
 
-const StyledSlider = styled(Slider)<{ variant?: 'cover'; customStyle?: CarouselProps }>`
+const StyledSlider = styled(Slider)<{ variant?: 'cover' }>`
   && {
     ${props =>
       props.variant === 'cover' &&
@@ -29,68 +27,15 @@ const StyledSlider = styled(Slider)<{ variant?: 'cover'; customStyle?: CarouselP
   }
 `
 
-type CarouselCover = {
-  title?: string
-  paragraph?: string
-  desktopCoverUrl: string
-  mobileCoverUrl: string
-  link: string
-  openNewTab: boolean
-}
-
-type CarouselProps = {
-  type: 'normal' | 'simply'
-  covers: CarouselCover[]
-  dots?: boolean
-  arrows?: boolean
-  draggable?: boolean
-  swipeToSlide?: boolean
-  infinite?: boolean
-  autoplay?: boolean
-  speed?: number
-  slidesToShow?: number
-  slidesToScroll?: number
-  autoplaySpeed?: number
-}
+type CarouselProps = SliderProps
 
 const Carousel: ElementComponent<CarouselProps> = props => {
-  const history = useHistory()
   if (props.loading || props.errors) {
     return null
   }
   return (
-    <StyledSlider
-      dots={props.dots}
-      draggable={props.draggable}
-      swipeToSlide={props.swipeToSlide}
-      infinite={props.infinite}
-      speed={props.speed}
-      arrows={props.arrows}
-      autoplay={props.autoplay}
-      autoplaySpeed={props.autoplaySpeed}
-      slidesToShow={props.slidesToShow}
-      slidesToScroll={props.slidesToScroll}
-      variant="cover"
-    >
-      {props.covers.map(cover => (
-        <Slide
-          key={cover.title}
-          srcDesktop={cover.desktopCoverUrl}
-          srcMobile={cover.mobileCoverUrl}
-          title={cover.title}
-          subtitle={cover.paragraph}
-          onClick={() => {
-            if (props.editing || !cover.link) {
-              return
-            }
-            cover.openNewTab
-              ? window.open(cover.link)
-              : cover.link.includes('http')
-              ? window.location.assign(cover.link)
-              : history.push(cover.link)
-          }}
-        />
-      ))}
+    <StyledSlider {...props} variant="cover">
+      {props.children}
     </StyledSlider>
   )
 }
