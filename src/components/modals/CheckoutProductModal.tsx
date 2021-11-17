c1d60f4f48f1f134839ba4467040b2d163dbe487
ponchimeow <trancetoradora@gmail.com>
2021-11-17 15:48:04 +0800

    [FEATURE] add test code

--- src/components/modals/CheckoutProductModal.tsx
+++ src/components/modals/CheckoutProductModal.tsx
@@ -250,7 +250,13 @@ const CheckoutProductModal: React.VFC<CheckoutProductModalProps> = ({
   const [referrerEmail, setReferrerEmail] = useState('')
   const { memberId: referrerId, validateStatus: referrerStatus } = useMemberValidation(referrerEmail)
   const updateMemberMetadata = useUpdateMemberMetadata()
-
+  console.log('************************')
+  console.log(currentMember)
+  console.log('-------')
+  console.log(target)
+  console.log('-------')
+  console.log(payment)
+  console.log('************************')
   if (currentMember === null || target === null || payment === undefined) {
     return renderTrigger?.({ isLoading: true })
   }
