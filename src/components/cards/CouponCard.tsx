4c774ba3bdcbad6d56236988c77e52131a2f4065
ponchimeow <trancetoradora@gmail.com>
2022-04-08 17:52:22 +0800

    [BUGFIX] add empty element

--- src/components/cards/CouponCard.tsx
+++ src/components/cards/CouponCard.tsx
@@ -22,37 +22,39 @@ const CouponCard: React.VFC<
 
   return (
     <BoxWrapper {...boxProps} mb="12px" p="24px">
-      <div style={{ fontSize: '20px', fontWeight: 'bold', paddingBottom: '12px' }}>
-        {coupon.couponCode.couponPlan.title}
-      </div>
-      <div>
-        {coupon.couponCode.couponPlan.constraint
-          ? formatMessage(
-              { id: 'checkout.coupon.full', defaultMessage: '消費滿 {amount} 折抵' },
-              { amount: <PriceLabel listPrice={coupon.couponCode.couponPlan.constraint} /> },
-            )
-          : formatMessage(checkoutMessages.content.discountDirectly)}
-        {coupon.couponCode.couponPlan.type === 'cash'
-          ? formatMessage(
-              { id: 'checkout.coupon.amount', defaultMessage: '金額 {amount} 元' },
-              { amount: <PriceLabel listPrice={coupon.couponCode.couponPlan.amount} /> },
-            )
-          : coupon.couponCode.couponPlan.type === 'percent'
-          ? formatMessage(
-              { id: 'checkout.coupon.proportion', defaultMessage: '比例 {amount}%' },
-              { amount: coupon.couponCode.couponPlan.amount },
-            )
-          : ''}
-      </div>
-      <div>
-        {coupon.couponCode.couponPlan.startedAt
-          ? dateFormatter(coupon.couponCode.couponPlan.startedAt)
-          : formatMessage(checkoutMessages.coupon.fromNow)}
-        {' ~ '}
-        {coupon.couponCode.couponPlan.endedAt
-          ? dateFormatter(coupon.couponCode.couponPlan.endedAt)
-          : formatMessage(checkoutMessages.coupon.noPeriod)}
-      </div>
+      <>
+        <div style={{ fontSize: '20px', fontWeight: 'bold', paddingBottom: '12px' }}>
+          {coupon.couponCode.couponPlan.title}
+        </div>
+        <div>
+          {coupon.couponCode.couponPlan.constraint
+            ? formatMessage(
+                { id: 'checkout.coupon.full', defaultMessage: '消費滿 {amount} 折抵' },
+                { amount: <PriceLabel listPrice={coupon.couponCode.couponPlan.constraint} /> },
+              )
+            : formatMessage(checkoutMessages.content.discountDirectly)}
+          {coupon.couponCode.couponPlan.type === 'cash'
+            ? formatMessage(
+                { id: 'checkout.coupon.amount', defaultMessage: '金額 {amount} 元' },
+                { amount: <PriceLabel listPrice={coupon.couponCode.couponPlan.amount} /> },
+              )
+            : coupon.couponCode.couponPlan.type === 'percent'
+            ? formatMessage(
+                { id: 'checkout.coupon.proportion', defaultMessage: '比例 {amount}%' },
+                { amount: coupon.couponCode.couponPlan.amount },
+              )
+            : ''}
+        </div>
+        <div>
+          {coupon.couponCode.couponPlan.startedAt
+            ? dateFormatter(coupon.couponCode.couponPlan.startedAt)
+            : formatMessage(checkoutMessages.coupon.fromNow)}
+          {' ~ '}
+          {coupon.couponCode.couponPlan.endedAt
+            ? dateFormatter(coupon.couponCode.couponPlan.endedAt)
+            : formatMessage(checkoutMessages.coupon.noPeriod)}
+        </div>
+      </>
     </BoxWrapper>
   )
 }
