60c2feedc01d2f6d0b1583c098784052f3cb3623
emily40830 <emily40830@gmail.com>
2022-04-19 21:10:47 +0800

    feat: move utm_source to memo component

--- src/components/common/Tracking.tsx
+++ src/components/common/Tracking.tsx
@@ -8,38 +8,36 @@ const Impression: React.FC<{
   resources: (Resource | null)[]
   collection?: string
   ignore?: 'EEC' | 'CUSTOM'
-  utmSource?: string
-}> = React.memo(({ resources, collection, ignore, utmSource }) => {
+}> = React.memo(({ resources, collection, ignore }) => {
   const tracking = useTracking()
+  const [utmSource] = useQueryParam('utm_source', StringParam)
   useEffect(() => {
     tracking.impress(resources, { collection, ignore, utmSource: utmSource || '' })
   }, [collection, resources, tracking])
   return <></>
 }, equals)
 
-const Detail: React.FC<{ resource: Resource; ignore?: 'EEC' | 'CUSTOM'; utmSource?: string }> = React.memo(
-  ({ resource, ignore, utmSource }) => {
-    const [pageFrom] = useQueryParam('pageFrom', StringParam)
-    const tracking = useTracking()
-    useEffect(() => {
-      tracking.detail(resource, { collection: pageFrom || undefined, ignore, utmSource: utmSource || '' })
-    }, [pageFrom, utmSource, resource, tracking, ignore])
-    return <></>
-  },
-  equals,
-)
+const Detail: React.FC<{ resource: Resource; ignore?: 'EEC' | 'CUSTOM' }> = React.memo(({ resource, ignore }) => {
+  const [pageFrom] = useQueryParam('pageFrom', StringParam)
+  const [utmSource] = useQueryParam('utm_source', StringParam)
+  const tracking = useTracking()
+  useEffect(() => {
+    tracking.detail(resource, { collection: pageFrom || undefined, ignore, utmSource: utmSource || '' })
+  }, [pageFrom, utmSource, resource, tracking, ignore])
+  return <></>
+}, equals)
 
 const Checkout: React.FC<{
   resources: Resource[]
   onCheckout?: () => void
   ignore?: 'EEC' | 'CUSTOM'
-  utmSource?: string
-}> = React.memo(({ resources, onCheckout, ignore, utmSource }) => {
+}> = React.memo(({ resources, onCheckout, ignore }) => {
   const tracking = useTracking()
+  const [utmSource] = useQueryParam('utm_source', StringParam)
   useEffect(() => {
     tracking.checkout(resources, { ignore, utmSource: utmSource || '' })
     onCheckout?.()
-  }, [onCheckout, utmSource, resources, tracking, ignore])
+  }, [onCheckout, resources, tracking, ignore])
   return <></>
 }, equals)
 
@@ -49,9 +47,9 @@ const Purchase: React.FC<{
   discounts: { name: string; price: number }[]
   ignore?: 'EEC' | 'CUSTOM'
   onTracked?: () => void
-  utmSource?: string
-}> = React.memo(({ orderId, products, discounts, ignore, onTracked, utmSource }) => {
+}> = React.memo(({ orderId, products, discounts, ignore, onTracked }) => {
   const tracking = useTracking()
+  const [utmSource] = useQueryParam('utm_source', StringParam)
   useEffect(() => {
     tracking.purchase(orderId, products, discounts, { ignore, utmSource: utmSource || '' })
     onTracked?.()
