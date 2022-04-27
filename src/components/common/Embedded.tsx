c3408c2db730f9d2732a4c9365d174f6ae5241f5
ponchimeow <trancetoradora@gmail.com>
2022-04-27 11:25:28 +0800

    [BUGFIX] use window location href

--- src/components/common/Embedded.tsx
+++ src/components/common/Embedded.tsx
@@ -20,8 +20,7 @@ const Embedded: ElementComponent<EmbeddedProps> = props => {
     const targetLink = e.target.closest('a')
     if (!targetLink) return
     e.preventDefault()
-    console.log(targetLink.href)
-    history.push(targetLink.href)
+    window.location.href = targetLink.href
   }
 
   return (
