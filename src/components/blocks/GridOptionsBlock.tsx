05dd1507f92c82da6327ac412c09ab9dbfb79878
Sam Ho <twhltvproxy@gmail.com>
2022-08-22 16:32:30 +0800

    [FEATURE] add question options blocks

---
+++ src/components/blocks/GridOptionsBlock.tsx
@@ -0,0 +1,45 @@
+import { Grid, GridItem } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { QuestionOption } from '../../types/question_library'
+
+const GridOption = styled.div<{ font: string; selected: boolean }>`
+  position: relative;
+  width: 100%;
+  aspect-ratio: 1/1;
+  outline: ${props => (props.selected ? '2px solid var(--gray-darker)' : '1px solid var(--gray)')};
+  border-radius: 4px;
+  padding: 20px;
+  font-family: ${props => (props.font === 'zhuyin' ? 'BpmfGenSenRounded' : 'inherit')};
+  font-size: ${props => (props.font === 'zhuyin' ? '32px' : '16px')};
+  img {
+    object-fit: contain;
+    aspect-ratio: 1;
+  }
+`
+
+const GridOptionsBlock: React.VFC<{ optionList?: QuestionOption[]; questionFontType?: string }> = ({
+  optionList,
+  questionFontType = 'auto',
+}) => {
+  const [selectedOptionId, setSelectedOptionId] = useState<string>('')
+  return (
+    (optionList && (
+      <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+        {optionList?.map(option => (
+          <GridItem key={option.id} colSpan={1} w="100%">
+            <GridOption
+              dangerouslySetInnerHTML={{ __html: option.value }}
+              font={questionFontType}
+              selected={selectedOptionId === option.id ? true : false}
+              onClick={() => setSelectedOptionId(option.id)}
+            />
+          </GridItem>
+        ))}
+      </Grid>
+    )) ||
+    null
+  )
+}
+
+export default GridOptionsBlock
---
+++ src/components/blocks/ListsOptionsBlock.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import checkIconSrc from '../../images/icons/check.svg'
+import { QuestionOption } from '../../types/question_library'
+
+const ListsOption = styled.div<{ font: string; selected: boolean }>`
+  position: relative;
+  padding: 20px;
+  margin-bottom: 16px;
+  font-weight: 500;
+  line-height: 1.5;
+  letter-spacing: 0.2px;
+  color: #585858;
+  border: 1px solid ${props => (props.selected ? 'var(--gray-darker)' : 'var(--gray)')};
+  border-radius: 4px;
+  font-family: ${props => (props.font === 'zhuyin' ? 'BpmfGenSenRounded' : 'inherit')};
+  font-size: ${props => (props.font === 'zhuyin' ? '32px' : '16px')};
+  img {
+    object-fit: contain;
+    aspect-ratio: 1;
+  }
+  &:after {
+    content: '';
+    display: ${props => (props.selected ? 'block' : 'none')};
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    top: 50%;
+    right: 20px;
+    transform: translateY(-50%);
+    background-image: url(${checkIconSrc});
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+`
+
+const ListsOptionsBlock: React.VFC<{ optionList?: QuestionOption[]; questionFontType?: string }> = ({
+  optionList,
+  questionFontType = 'auto',
+}) => {
+  const [selectedOptionId, setSelectedOptionId] = useState<string>('')
+  return (
+    (optionList && (
+      <>
+        {optionList?.map(option => (
+          <ListsOption
+            key={option.id}
+            dangerouslySetInnerHTML={{ __html: option.value }}
+            font={questionFontType}
+            selected={selectedOptionId === option.id ? true : false}
+            onClick={() => setSelectedOptionId(option.id)}
+          />
+        ))}
+      </>
+    )) ||
+    null
+  )
+}
+
+export default ListsOptionsBlock
