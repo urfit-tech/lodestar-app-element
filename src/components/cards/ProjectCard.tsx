d655cb7ae2a752f63a128fda922390453554888e
sunnylochun <lo@qraft.app>
2022-10-31 18:34:04 +0800

    [STYLE] project card style adjustment

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -56,6 +56,25 @@ const ProjectCard: React.FC<ProjectElementProps> = props => {
     return <div>{JSON.stringify(errors)}</div>
   }
 
+  const TitleElement = ({ style }: { style?: React.CSSProperties }) => (
+    <Card.Title
+      style={{
+        fontSize: '18',
+        textAlign: 'left',
+        fontWeight: 'bold',
+        color: '',
+        ...style,
+      }}
+      className="content__title"
+    >
+      {loading ? (
+        <Skeleton height={4} width={100} />
+      ) : (
+        <Link to={loading ? `#!` : `/projects/${props.id}`}>{props.title}</Link>
+      )}
+    </Card.Title>
+  )
+
   return (
     <div
       onClick={e => {
@@ -68,126 +87,135 @@ const ProjectCard: React.FC<ProjectElementProps> = props => {
             className="cover"
             width="100%"
             ratio={9 / 16}
+            shape="rounded"
             src={loading ? EmptyCover : props.previewUrl || props.coverUrl || EmptyCover}
           />
           {props.type === 'portfolio' && props.coverType === 'video' && <StyledPlayIcon src={PlayIcon} />}
         </StyledCover>
-        <Card.Content className="content">
-          <Card.Title
-            style={{ height: '3rem', fontSize: '18', textAlign: 'left', fontWeight: 'bold', color: '' }}
-            className="mb-3 content__title"
-          >
-            {loading ? (
-              <Skeleton height={4} width={100} />
-            ) : (
-              <Link to={loading ? `#!` : `/projects/${props.id}`}>{props.title}</Link>
-            )}
-          </Card.Title>
-          {props.type !== 'portfolio' && (
-            <>
-              <Card.Description className="mb-3 description">
-                {loading ? (
-                  <SkeletonText noOfLines={5} />
-                ) : (
-                  <span className="description__abstract">{props.abstract}</span>
+        {props.type === 'portfolio' && (
+          <TitleElement
+            style={{
+              alignSelf: 'start',
+              marginBottom: '0.75rem',
+              marginTop: '1.125rem',
+              display: '-webkit-box',
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: 'vertical',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+            }}
+          />
+        )}
+        {props.type !== 'portfolio' && (
+          <Card.Content className="content">
+            <TitleElement
+              style={{
+                height: '3rem',
+                marginBottom: '1rem',
+              }}
+            />
+            <Card.Description className="mb-3 description">
+              {loading ? (
+                <SkeletonText noOfLines={5} />
+              ) : (
+                <span className="description__abstract">{props.abstract}</span>
+              )}
+            </Card.Description>
+            <Card.MetaBlock className="metadata d-flex align-items-end justify-content-between">
+              <StyledCircleWrapper className="targetLeft">
+                {!loading && props.type === 'funding' && (
+                  <StyledPercent className="percent">
+                    <span className="percent__amount">
+                      {!props.targetAmount
+                        ? 0
+                        : Math.floor(
+                            (((props.targetUnit === 'participants' ? props.enrollmentCount : props.totalSales) || 0) *
+                              100) /
+                              props.targetAmount,
+                          )}
+                    </span>
+                    <span className="percent__unit">%</span>
+                  </StyledPercent>
                 )}
-              </Card.Description>
-              <Card.MetaBlock className="metadata d-flex align-items-end justify-content-between">
-                <StyledCircleWrapper className="targetLeft">
-                  {!loading && props.type === 'funding' && (
-                    <StyledPercent className="percent">
-                      <span className="percent__amount">
-                        {!props.targetAmount
-                          ? 0
-                          : Math.floor(
-                              (((props.targetUnit === 'participants' ? props.enrollmentCount : props.totalSales) || 0) *
-                                100) /
-                                props.targetAmount,
-                            )}
-                      </span>
-                      <span className="percent__unit">%</span>
-                    </StyledPercent>
-                  )}
-                  {!loading && props.type === 'funding' ? (
-                    <Circle
-                      className="target__circle"
-                      percent={
-                        !props.targetAmount
-                          ? 0
-                          : Math.floor(
-                              (((props.targetUnit === 'participants' ? props.enrollmentCount : props.totalSales) || 0) *
-                                100) /
-                                props.targetAmount,
-                            )
-                      }
-                      style={{ width: 50 }}
-                      trailWidth={12}
-                      trailColor="#ececec"
-                      strokeWidth={12}
-                      strokeColor={theme['@primary-color']}
-                    />
-                  ) : !loading && props.isParticipantsVisible ? (
-                    <div className="d-flex align-items-center participants">
-                      <UserOIcon className="participants__userIcon" />
-                      <span className="participants__enrollmentCount">
-                        {formatMessage(projectMessages.text.people, { count: loading ? 0 : props.enrollmentCount })}
-                      </span>
-                    </div>
-                  ) : null}
-                </StyledCircleWrapper>
+                {!loading && props.type === 'funding' ? (
+                  <Circle
+                    className="target__circle"
+                    percent={
+                      !props.targetAmount
+                        ? 0
+                        : Math.floor(
+                            (((props.targetUnit === 'participants' ? props.enrollmentCount : props.totalSales) || 0) *
+                              100) /
+                              props.targetAmount,
+                          )
+                    }
+                    style={{ width: 50 }}
+                    trailWidth={12}
+                    trailColor="#ececec"
+                    strokeWidth={12}
+                    strokeColor={theme['@primary-color']}
+                  />
+                ) : !loading && props.isParticipantsVisible ? (
+                  <div className="d-flex align-items-center participants">
+                    <UserOIcon className="participants__userIcon" />
+                    <span className="participants__enrollmentCount">
+                      {formatMessage(projectMessages.text.people, { count: loading ? 0 : props.enrollmentCount })}
+                    </span>
+                  </div>
+                ) : null}
+              </StyledCircleWrapper>
 
-                <div className="text-right targetRight">
-                  {loading && <Skeleton height={4} width={20} />}
-                  {!loading && props.type === 'funding' && (
-                    <StyledLabel className="participants">
-                      {props.targetUnit === 'participants' && (
-                        <span className="participants__totalParticipantsText">
-                          {formatMessage(projectMessages.text.totalParticipants, { count: props.enrollmentCount })}
-                        </span>
-                      )}
-                      {props.targetUnit === 'funds' && <PriceLabel listPrice={props.totalSales || 0} />}
-                    </StyledLabel>
-                  )}
-                  {!loading && props.isCountdownTimerVisible && props.expiredAt && (
-                    <>
-                      {moment().isAfter(props.expiredAt) ? (
-                        <div className="d-flex align-items-center justify-content-end expiredDate">
-                          <CalendarAltOIcon className="mr-1" />
-                          {props.type === 'funding' ? (
-                            <span className="expiredDate__isExpiredFundingText">
-                              {formatMessage(projectMessages.label.isExpiredFunding)}
-                            </span>
-                          ) : (
-                            <span className="expiredDate__isExpiredText">
-                              {formatMessage(projectMessages.label.isExpired)}
-                            </span>
-                          )}
-                        </div>
-                      ) : (
-                        <StyledLabel className="d-flex align-items-center countDownDays">
-                          <CalendarAltOIcon className="mr-1 countDownDays__calendarIcon" />
-                          {props.type === 'funding' ? (
-                            <span className="countDownDays__fundingCountDownDaysText">
-                              {formatMessage(projectMessages.text.fundingCountDownDays, {
-                                days: moment(props.expiredAt).diff(new Date(), 'days'),
-                              })}
-                            </span>
-                          ) : (
-                            <span className="countDownDays__preOrderCountDownDaysText">
-                              {formatMessage(projectMessages.text.preOrderCountDownDays, {
-                                days: moment(props.expiredAt).diff(new Date(), 'days'),
-                              })}
-                            </span>
-                          )}
-                        </StyledLabel>
-                      )}
-                    </>
-                  )}
-                </div>
-              </Card.MetaBlock>
-            </>
-          )}
-        </Card.Content>
+              <div className="text-right targetRight">
+                {loading && <Skeleton height={4} width={20} />}
+                {!loading && props.type === 'funding' && (
+                  <StyledLabel className="participants">
+                    {props.targetUnit === 'participants' && (
+                      <span className="participants__totalParticipantsText">
+                        {formatMessage(projectMessages.text.totalParticipants, { count: props.enrollmentCount })}
+                      </span>
+                    )}
+                    {props.targetUnit === 'funds' && <PriceLabel listPrice={props.totalSales || 0} />}
+                  </StyledLabel>
+                )}
+                {!loading && props.isCountdownTimerVisible && props.expiredAt && (
+                  <>
+                    {moment().isAfter(props.expiredAt) ? (
+                      <div className="d-flex align-items-center justify-content-end expiredDate">
+                        <CalendarAltOIcon className="mr-1" />
+                        {props.type === 'funding' ? (
+                          <span className="expiredDate__isExpiredFundingText">
+                            {formatMessage(projectMessages.label.isExpiredFunding)}
+                          </span>
+                        ) : (
+                          <span className="expiredDate__isExpiredText">
+                            {formatMessage(projectMessages.label.isExpired)}
+                          </span>
+                        )}
+                      </div>
+                    ) : (
+                      <StyledLabel className="d-flex align-items-center countDownDays">
+                        <CalendarAltOIcon className="mr-1 countDownDays__calendarIcon" />
+                        {props.type === 'funding' ? (
+                          <span className="countDownDays__fundingCountDownDaysText">
+                            {formatMessage(projectMessages.text.fundingCountDownDays, {
+                              days: moment(props.expiredAt).diff(new Date(), 'days'),
+                            })}
+                          </span>
+                        ) : (
+                          <span className="countDownDays__preOrderCountDownDaysText">
+                            {formatMessage(projectMessages.text.preOrderCountDownDays, {
+                              days: moment(props.expiredAt).diff(new Date(), 'days'),
+                            })}
+                          </span>
+                        )}
+                      </StyledLabel>
+                    )}
+                  </>
+                )}
+              </div>
+            </Card.MetaBlock>
+          </Card.Content>
+        )}
       </Card>
       {props.type === 'portfolio' && (
         <InstructorPlaceHolder>
