9e7e3aaeae90c85a842df023829ef49ab0336fa0
ponchimeow <trancetoradora@gmail.com>
2024-05-07 17:51:21 +0800

    feat:export empty cover

--- src/images/index.tsx
+++ src/images/index.tsx
@@ -1,6 +1,7 @@
 export { ReactComponent as CalendarAltOIcon } from './calendar-alt-o.svg'
 export { ReactComponent as CheckedCircleIcon } from './checked-circle.svg'
 export { ReactComponent as DefaultAvatarIcon } from './default-avatar.svg'
+export { ReactComponent as EmptyCover } from './empty-cover.svg'
 export { ReactComponent as ExclamationCircleIcon } from './exclamation-circle.svg'
 export { ReactComponent as PlayCircle, ReactComponent as PlayCircleIcon } from './play-circle.svg'
 export { ReactComponent as PlayFill } from './play-fill.svg'
