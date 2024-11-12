ce78e1be77bbf0bf2698e9f61d4133d52c8f230c
Logos as a philosopher <yures611@gmail.com>
2024-11-12 13:41:23 +0800

    feat: useReviewable

--- src/hooks/review.ts
+++ src/hooks/review.ts
@@ -1,6 +1,20 @@
 import { gql, useQuery } from '@apollo/client'
 import hasura from '../hasura'
 
+export const useReviewable = (path: string, appId: string) =>
+  useQuery<hasura.GET_REVIEWABLE, hasura.GET_REVIEWABLEVariables>(
+    gql`
+      query GET_REVIEWABLE($path: String!, $appId: String!) {
+        reviewable(where: { path: { _eq: $path }, app_id: { _eq: $appId } }) {
+          is_item_viewable
+          is_score_viewable
+          is_writable
+        }
+      }
+    `,
+    { variables: { path, appId } },
+  )
+
 export const useReviewAggregate = (path: string) => {
   const { loading, error, data, refetch } = useQuery<hasura.GET_REVIEW_AGGREGATE, hasura.GET_REVIEW_AGGREGATEVariables>(
     gql`
