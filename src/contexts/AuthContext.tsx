ee0a0a9aa5d4670cfc03937afd59f8558dc77e88
ponchimeow <trancetoradora@gmail.com>
2025-01-07 16:43:30 +0800

    feat:fingerPrintId and geoLocation

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -248,13 +248,15 @@ export const AuthProvider: React.FC<{ appId: string }> = ({ appId, children }) =
             }
           }),
         login: async ({ account, password, accountLinkToken }) => {
+          const fingerPrintId = await getFingerPrintId()
+          const { ip, country, countryCode } = await fetchCurrentGeolocation()
           try {
             const {
               data: { code, message, result },
             } = await Axios.post(
               // `${process.env.REACT_APP_API_BASE_ROOT}/auth/general-login`,
               `http://localhost:8081/api/v2/auth/general-login`,
-              { appId, account, password },
+              { appId, account, password, fingerPrintId, geoLocation: { ip, country, countryCode } },
               { withCredentials: true },
             )
 
