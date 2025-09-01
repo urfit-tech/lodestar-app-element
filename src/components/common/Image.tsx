32fe9d1f401baf1bea95d515506dcb10311852a8
ponchimeow <trancetoradora@gmail.com>
2025-09-01 12:03:09 +0800

    fix: remove console

--- src/components/common/Image.tsx
+++ src/components/common/Image.tsx
@@ -56,7 +56,7 @@ const Image: ElementComponent<ImageProps & { customStyle?: CSSObject }> = props 
   const getBackgroundImage = () => {
     const width = window.innerWidth
     const extractUrl = (bg?: string) => bg?.replace(/^url\(["']?/, '').replace(/["']?\)$/, '')
-    console.log('props.backgroundImages', props?.backgroundImages)
+
     if (width >= DESKTOP_BREAK_POINT) {
       return extractUrl(
         props?.backgroundImages?.desktop || props?.backgroundImages?.tablet || props?.backgroundImages?.mobile || '',
