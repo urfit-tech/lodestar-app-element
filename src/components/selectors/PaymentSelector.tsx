dc2cb64f6bff7282d45949626097def66b98ec7e
Logos as a philosopher <yures611@gmail.com>
2026-04-10 11:49:37 +0800

    feat: 依據 payment.payment_gateway_mask 設定篩選支付選項

--- src/components/selectors/PaymentSelector.tsx
+++ src/components/selectors/PaymentSelector.tsx
@@ -5,6 +5,7 @@ import { Form } from 'antd'
 import React, { useState } from 'react'
 import { useIntl } from 'react-intl'
 import styled from 'styled-components'
+import { useApp } from '../../contexts/AppContext'
 import * as hasura from '../../hasura'
 import { checkoutMessages } from '../../helpers/translation'
 import { PaymentGatewayType, PaymentMethodType, PaymentProps } from '../../types/checkout'
@@ -73,11 +74,21 @@ const PaymentSelector: React.FC<{
 }
 
 const getPaymentOptions = () => {
+  const { settings } = useApp()
+  const availablePaymentGateways = JSON.parse(settings['AVAILABLE_PAYMENT_GATEWAYS'] || '[]')
+  const maskedGateways: string[] = settings['payment.payment_gateway_mask']
+    ? JSON.parse(settings['payment.payment_gateway_mask'])
+    : []
   const { formatMessage } = useIntl()
   const { data, loading, error } = useQuery<hasura.getPaymentGatewayMethod>(
     gql`
-      query getPaymentGatewayMethod {
-        app_payment_gateway_method(where: { status: { _eq: "enabled" } }) {
+      query getPaymentGatewayMethod($availablePaymentGateways: [String!]!) {
+        app_payment_gateway_method(
+          where: {
+            status: { _eq: "enabled" }
+            app_payment_gateway: { app_id: { _eq: "tli1956" }, gateway: { name: { _in: $availablePaymentGateways } } }
+          }
+        ) {
           method {
             name
           }
@@ -89,6 +100,7 @@ const getPaymentOptions = () => {
         }
       }
     `,
+    { variables: { availablePaymentGateways } },
   )
 
   const paymentOptions = data?.app_payment_gateway_method.reduce<{
@@ -127,7 +139,7 @@ const getPaymentOptions = () => {
     { methodCount: {}, paymentOptions: [] },
   ).paymentOptions
 
-  return paymentOptions
+  return paymentOptions?.filter(option => !maskedGateways.includes(option.payment.gateway))
 }
 
 export default PaymentSelector
